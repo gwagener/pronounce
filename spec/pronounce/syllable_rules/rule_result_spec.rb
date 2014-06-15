c14dcea8c8a1ed85c58214c8019741f0fca5c3d4
Graham Wagener <graham.wagener@gmail.com>
2014-06-15 22:45:23 +1200

    Reduced duplication with some lets.

--- spec/pronounce/syllable_rules/rule_result_spec.rb
+++ spec/pronounce/syllable_rules/rule_result_spec.rb
@@ -3,24 +3,28 @@ require 'pronounce/syllable_rules/rule_result'
 module Pronounce::SyllableRules
   describe RuleResult do
     describe '#<=>' do
+      let(:new_syllable_result) { RuleResult.new(:new_syllable, 0) }
+      let(:no_new_syllable_result) { RuleResult.new(:no_new_syllable, 0) }
+      let(:not_application_result) { RuleResult.new(:not_applicable, 0) }
+      let(:later_new_syllable_result) { RuleResult.new(:new_syllable, 1) }
+
       it 'ranks results with the same parameters equally' do
-        expect(RuleResult.new(:new_syllable, 0)).to eq RuleResult.new(:new_syllable, 0)
+        expect(new_syllable_result).to eq RuleResult.new(:new_syllable, 0)
       end
 
       it 'ranks :new_syllable > :no_new_syllable > :not_applicable' do
-        expect(RuleResult.new(:new_syllable, 0)).to be > RuleResult.new(:no_new_syllable, 0)
-        expect(RuleResult.new(:no_new_syllable, 0)).to be > RuleResult.new(:not_applicable, 0)
-        expect(RuleResult.new(:not_applicable, 0)).to be < RuleResult.new(:new_syllable, 0)
+        expect(new_syllable_result).to be > no_new_syllable_result
+        expect(no_new_syllable_result).to be > not_application_result
+        expect(not_application_result).to be < new_syllable_result
       end
 
       it 'ranks results that accessed earlier phones higher than other applicable results' do
-        expect(RuleResult.new(:no_new_syllable, 0)).to be > RuleResult.new(:new_syllable, 1)
-        expect(RuleResult.new(:not_applicable, 0)).to be < RuleResult.new(:no_new_syllable, 1)
+        expect(no_new_syllable_result).to be > later_new_syllable_result
+        expect(not_application_result).to be < later_new_syllable_result
       end
 
       it 'fails when trying to compare to a non-RuleResult' do
-        result = RuleResult.new(:new_syllable, 0)
-        expect { result > :no_new_syllable }.to raise_error ArgumentError
+        expect { new_syllable_result > :no_new_syllable }.to raise_error ArgumentError
       end
     end
 
--- spec/pronounce/syllable_rules/rule_set_result_spec.rb
+++ spec/pronounce/syllable_rules/rule_set_result_spec.rb
@@ -3,22 +3,29 @@ require 'pronounce/syllable_rules/rule_set_result'
 module Pronounce::SyllableRules
   describe RuleSetResult do
     describe '#<=>' do
+      let(:new_syllable_result) { RuleSetResult.new(:lang, :new_syllable) }
+      let(:no_new_syllable_result) { RuleSetResult.new(:lang, :no_new_syllable) }
+      let(:not_application_result) { RuleSetResult.new(:lang, :not_applicable) }
+      let(:base_new_syllable_result) { RuleSetResult.new(:base, :new_syllable) }
+
+      it 'ranks results with the same parameters equally' do
+        expect(new_syllable_result).to eq RuleSetResult.new(:lang, :new_syllable)
+      end
+
       it 'ranks :new_syllable > :no_new_syllable > :not_applicable' do
-        expect(RuleSetResult.new(:lang, :new_syllable)).to eq RuleSetResult.new(:lang, :new_syllable)
-        expect(RuleSetResult.new(:lang, :new_syllable)).to be > RuleSetResult.new(:lang, :no_new_syllable)
-        expect(RuleSetResult.new(:lang, :no_new_syllable)).to be > RuleSetResult.new(:lang, :not_applicable)
-        expect(RuleSetResult.new(:lang, :not_applicable)).to be < RuleSetResult.new(:lang, :new_syllable)
+        expect(new_syllable_result).to be > no_new_syllable_result
+        expect(no_new_syllable_result).to be > not_application_result
+        expect(not_application_result).to be < new_syllable_result
       end
 
       it 'ranks results of base rules lower than other applicable results' do
-        expect(RuleSetResult.new('a rule', :new_syllable)).to eq RuleSetResult.new('another rule', :new_syllable)
-        expect(RuleSetResult.new(:lang, :no_new_syllable)).to be > RuleSetResult.new(:base, :new_syllable)
-        expect(RuleSetResult.new(:lang, :not_applicable)).to be < RuleSetResult.new(:base, :no_new_syllable)
+        expect(new_syllable_result).to eq RuleSetResult.new(:other_lang, :new_syllable)
+        expect(no_new_syllable_result).to be > base_new_syllable_result
+        expect(not_application_result).to be < base_new_syllable_result
       end
 
       it 'fails when trying to compare to a non-RuleSetResult' do
-        result = RuleSetResult.new(:lang, :new_syllable)
-        expect { result > :no_new_syllable }.to raise_error ArgumentError
+        expect { new_syllable_result > :no_new_syllable }.to raise_error ArgumentError
       end
     end
 
